097c19443f61f5892969e40ed60a6b3baab05349
Neeharikaranam <92419190+Neeharikaranam@users.noreply.github.com>
2023-04-20 12:38:45 -0500

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,370 @@
+import collections
+
+import transformers
+import torch
+import torch.nn as nn
+
+import utils
+import layer
+import layer_utils
+
+
+class QAModel(nn.Module):
+    """
+    Abstract question answering module
+    """
+
+    IGNORE_LAYERS = []
+
+    def __init__(self):
+        super().__init__()
+
+    def count_parameters(self):
+        """
+        Return the total number of trainable parameters in the model
+        """
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    def state_dict(self):
+        """
+        Override default state dict to ignore
+        saving pre-defined layers
+        """
+        st_dict = super().state_dict()
+        keys = set(st_dict.keys())
+        for l in self.IGNORE_LAYERS:
+            for k in st_dict.keys():
+                if k.startswith(l):
+                    keys.remove(k)
+        return collections.OrderedDict({k: v for k, v in st_dict.items() if k in keys})
+
+    def load_state_dict(self, state_dict, strict=False):
+        """
+        Override default state dict loading
+        to allow the non-strict way
+        """
+        return super().load_state_dict(state_dict, strict=strict)
+
+
+class QABaselineModel(QAModel):
+    """
+    Recurrent encoder with a naive version of attention
+    """
+
+    IGNORE_LAYERS = ["embedding.weight"]
+
+    def __init__(
+        self,
+        embedding_module,
+        hidden_size=100,
+        num_recurrent_layers=2,
+        bidirectional=False,
+        dropout_rate=0.2,
+        device="cpu",
+    ):
+        super().__init__()
+
+        # Embedding module
+        self.embedding = embedding_module
+        self.embedding_dimension = embedding_module.weight.shape[-1]
+
+        # Projection layer (from word embedding dimension to hidden size)
+        self.projection = nn.Linear(self.embedding_dimension, hidden_size)
+
+        # Recurrent encoder
+        self.recurrent_module = layer.LSTM(
+            hidden_size,
+            hidden_size,
+            batch_first=True,
+            num_layers=num_recurrent_layers,
+            bidirectional=bidirectional,
+            dropout=dropout_rate,
+        )
+
+        # Output layer
+        out_dim = hidden_size if not bidirectional else 2 * hidden_size
+        self.out_lstm = layer.LSTM(
+            out_dim, hidden_size, batch_first=True, bidirectional=bidirectional
+        )
+        self.output_layer = layer.QAOutput(
+            out_dim,
+            1,
+            dropout_rate=dropout_rate,
+            classifier_bias=True,
+            device=device,
+        )
+
+        # Transfer model to device
+        self.device = device
+        self.to(self.device)
+
+    def forward(self, **inputs):
+        # Embed questions and contexts separately
+        embedded_questions = self.embedding(inputs["question_ids"])
+        embedded_contexts = self.embedding(inputs["context_ids"])
+
+        # Project questions and contexts into a lower-dimensional space
+        hidden_questions = self.projection(embedded_questions)
+        hidden_contexts = self.projection(embedded_contexts)
+
+        # Call the recurrent encoder for questions and contexts separately
+        padded_questions, _ = self.recurrent_module(
+            hidden_questions, inputs["question_lenghts"]
+        )
+        padded_contexts, _ = self.recurrent_module(
+            hidden_contexts, inputs["context_lenghts"]
+        )
+
+        # Average all the hidden states of a single question so as
+        # to obtain one vector
+        average_questions = padded_questions.sum(dim=1) / inputs[
+            "question_lenghts"
+        ].view(-1, 1)
+
+        # Perform element-wise multiplication between the aggregated question
+        # vector and each vector in the context
+        start_input = padded_contexts * average_questions.unsqueeze(1).repeat(
+            1, padded_contexts.shape[1], 1
+        )
+        end_input, _ = self.out_lstm(start_input, inputs["context_lenghts"])
+        return self.output_layer(start_input, end_input, **inputs)
+
+
+class QABiDAFModel(QAModel):
+    """
+    Custom implementation of a BiDAF model:
+
+    "Bidirectional Attention Flow for Machine Comprehension",
+    Minjoon Seo, Aniruddha Kembhavi, Ali Farhadi, Hannaneh Hajishirzi
+    """
+
+    IGNORE_LAYERS = ["word_embedding.weight"]
+
+    def __init__(
+        self,
+        embedding_module,
+        hidden_size=100,
+        highway_depth=2,
+        dropout_rate=0.2,
+        device="cpu",
+    ):
+        super().__init__()
+
+        # Embedding module
+        self.word_embedding = embedding_module
+        self.word_embedding_dimension = embedding_module.weight.shape[-1]
+
+        # Projection layer (from word embedding dimension to hidden size)
+        self.projection = nn.Linear(
+            self.word_embedding_dimension, hidden_size, bias=False
+        )
+
+        # Highway network
+        self.highway_depth = highway_depth
+        self.highway = layer_utils.get_highway(
+            self.highway_depth, hidden_size, device=device
+        )
+
+        # Dropout module
+        self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(self.dropout_rate)
+
+        # Contextual embeddings
+        self.contextual_embedding = layer.LSTM(
+            hidden_size,
+            hidden_size,
+            batch_first=True,
+            num_layers=1,
+            bidirectional=True,
+        )
+
+        # Attention flow
+        self.attention = layer.AttentionFlow(2 * hidden_size, device=device)
+
+        # Modeling layer
+        self.modeling_layer = layer.LSTM(
+            8 * hidden_size,
+            hidden_size,
+            batch_first=True,
+            bidirectional=True,
+            num_layers=2,
+            dropout=self.dropout_rate,
+        )
+
+        # Output layer
+        self.out_lstm = layer.LSTM(
+            2 * hidden_size,
+            hidden_size,
+            batch_first=True,
+            bidirectional=True,
+        )
+        self.output_layer = layer.QAOutput(
+            10 * hidden_size,
+            1,
+            dropout_rate=self.dropout_rate,
+            classifier_bias=False,
+            device=device,
+        )
+
+        # Transfer model to device
+        self.device = device
+        self.to(self.device)
+
+    def forward(self, **inputs):
+        # Extract masks and lenghts from the inputs
+        questions_mask = inputs["question_attention_mask"]
+        contexts_mask = inputs["context_attention_mask"]
+        questions_lenght = inputs["question_lenghts"]
+        contexts_lenght = inputs["context_lenghts"]
+
+        # Embed questions and contexts separately
+        embedded_questions = self.dropout(self.word_embedding(inputs["question_ids"]))
+        embedded_contexts = self.dropout(self.word_embedding(inputs["context_ids"]))
+
+        # Project questions and contexts into a lower-dimensional space
+        hidden_questions = self.projection(embedded_questions)
+        hidden_contexts = self.projection(embedded_contexts)
+
+        # Call the highway network for questions and contexts separately
+        highway_questions = self.highway(hidden_questions)
+        highway_contexts = self.highway(hidden_contexts)
+
+        # Contextual embedding layer
+        contextual_questions, _ = self.contextual_embedding(
+            highway_questions, questions_lenght
+        )
+        contextual_contexts, _ = self.contextual_embedding(
+            highway_contexts, contexts_lenght
+        )
+
+        # Attention flow
+        query_aware_contexts = self.attention(
+            contextual_questions, contextual_contexts, questions_mask, contexts_mask
+        )
+
+        # Modeling layer and output layer
+        modeling, _ = self.modeling_layer(query_aware_contexts, contexts_lenght)
+        m2, _ = self.out_lstm(modeling, contexts_lenght)
+        return self.output_layer(
+            torch.cat([query_aware_contexts, modeling], dim=-1),
+            torch.cat([query_aware_contexts, m2], dim=-1),
+            **inputs,
+        )
+
+
+class QABertModel(QAModel):
+    """
+    BERT model wrapper, for question answering tasks
+
+    "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding",
+    Jacob Devlin, Ming-Wei Chang, Kenton Lee and Kristina Toutanova
+    """
+
+    BERT_OUTPUT_SIZE = 768
+    MODEL_TYPE = "bert-base-uncased"
+
+    def __init__(self, dropout_rate=0.2, device="cpu"):
+        super().__init__()
+
+        # BERT model
+        self.bert_model = self.get_model()
+
+        # Output layer
+        self.out_lstm = layer.LSTM(
+            self.BERT_OUTPUT_SIZE,
+            self.BERT_OUTPUT_SIZE,
+            batch_first=True,
+            bidirectional=False,
+        )
+        self.output_layer = layer.QAOutput(
+            self.BERT_OUTPUT_SIZE,
+            1,
+            dropout_rate=dropout_rate,
+            classifier_bias=False,
+            device=device,
+        )
+
+        # Transfer model to device
+        self.device = device
+        self.to(self.device)
+
+    def get_model(self):
+        """
+        Returns a pre-trained BERT model
+        """
+        return transformers.BertModel.from_pretrained(self.MODEL_TYPE)
+
+    def get_model_inputs(self, **inputs):
+        """
+        Return a subset of the given input dict,
+        to be used as inputs for the wrapped Transformer
+        (BERT takes "input_ids", "token_type_ids" and "attention_mask" as inputs)
+        """
+        return {
+            "input_ids": inputs["context_ids"],
+            "token_type_ids": inputs["context_type_ids"],
+            "attention_mask": inputs["attention_mask"],
+        }
+
+    def forward(self, **inputs):
+        bert_inputs = self.get_model_inputs(**inputs)
+        bert_outputs = self.bert_model(**bert_inputs)[0]
+        end_input, _ = self.out_lstm(
+            bert_outputs,
+            torch.tensor(bert_outputs.shape[1], device=self.device).repeat(
+                bert_outputs.shape[0]
+            ),
+        )
+        outputs = self.output_layer(bert_outputs, end_input, **inputs)
+        return outputs
+
+
+class QADistilBertModel(QABertModel):
+    """
+    DistilBERT model wrapper, for question answering tasks
+
+    "DistilBERT, a distilled version of BERT: smaller, faster, cheaper and lighter",
+    Victor Sanh, Lysandre Debut, Julien Chaumond, Thomas Wolf
+    """
+
+    MODEL_TYPE = "distilbert-base-uncased"
+
+    def __init__(self, dropout_rate=0.2, device="cpu"):
+        super().__init__(dropout_rate=dropout_rate, device=device)
+
+    def get_model(self):
+        """
+        Returns a pre-trained DistilBERT model
+        """
+        return transformers.DistilBertModel.from_pretrained(self.MODEL_TYPE)
+
+    def get_model_inputs(self, **inputs):
+        """
+        Return a subset of the given input dict,
+        to be used as inputs for the wrapped Transformer
+        (DistilBERT takes "input_ids" and "attention_mask" as inputs)
+        """
+        return {
+            "input_ids": inputs["context_ids"],
+            "attention_mask": inputs["attention_mask"],
+        }
+
+
+class QAElectraModel(QABertModel):
+    """
+    ELECTRA model wrapper, for question answering tasks
+
+    "ELECTRA: Pre-training Text Encoders as Discriminators Rather Than Generators",
+    Kevin Clark, Minh-Thang Luong, Quoc V. Le, Christopher Manning
+    """
+
+    MODEL_TYPE = "google/electra-base-discriminator"
+
+    def __init__(self, dropout_rate=0.2, device="cpu"):
+        super().__init__(dropout_rate=dropout_rate, device=device)
+
+    def get_model(self):
+        """
+        Returns a pre-trained ELECTRA model
+        """
+        return transformers.ElectraModel.from_pretrained(self.MODEL_TYPE)
---
+++ src/tokenizer.py
@@ -0,0 +1,332 @@
+from operator import attrgetter
+
+import torch
+from tokenizers import BertWordPieceTokenizer, Tokenizer
+from tokenizers.implementations import BaseTokenizer
+from tokenizers.models import WordLevel
+from tokenizers.normalizers import Lowercase, Sequence, Strip, StripAccents
+from tokenizers.pre_tokenizers import Punctuation
+from tokenizers.pre_tokenizers import Sequence as PreSequence
+from tokenizers.pre_tokenizers import Whitespace
+
+
+class SquadTokenizer:
+    """
+    Abstract tokenizer and data collator interface
+    for the SQuAD dataset
+    """
+
+    ENCODING_ATTR = [
+        "ids",
+        "type_ids",
+        "tokens",
+        "offsets",
+        "attention_mask",
+        "special_tokens_mask",
+        "overflowing",
+        "word_ids",
+    ]
+    ENCODING_ATTR_ID = {k: i for i, k in enumerate(ENCODING_ATTR)}
+    ATTRGETTER = attrgetter(*ENCODING_ATTR)
+
+    def __init__(self, device="cpu"):
+        self.device = device
+
+    def tokenize(self, inputs, entity=None, special=False):
+        """
+        Tokenize the given input, using the specified tokenizer
+        (optionally remove special tokens, like padding)
+        """
+        tokenizer = self.select_tokenizer(entity)
+        tokenizer_padding = tokenizer.padding
+        if not special:
+            tokenizer.no_padding()
+        outputs = tokenizer.encode_batch(inputs, add_special_tokens=special)
+        tokenizer.enable_padding(**tokenizer_padding)
+        return outputs
+
+    def detokenize(self, inputs, entity=None, special=True):
+        """
+        De-tokenize the given tokenized input, into
+        the original word sequence
+        """
+        tokenizer = self.select_tokenizer(entity)
+        return tokenizer.decode_batch(inputs, skip_special_tokens=not special)
+
+    def get_pad_token_id(self):
+        """
+        Return the ID of the token used for padding
+        """
+        tokenizer = self.select_tokenizer(entity="context")
+        return tokenizer.padding["pad_id"]
+
+    def find_tokenized_answer_indexes(self, offsets, starts, ends):
+        """
+        Given start and end word indices, return the corresponding
+        indices in the tokenized space
+        """
+        batch_size = len(starts)
+        max_answers = max([len(row) for row in starts])
+        indexes = torch.full((batch_size, max_answers, 2), -100, device=self.device)
+        for i, (start, end) in enumerate(zip(starts, ends)):
+            for j, (s, e) in enumerate(zip(start, end)):
+                start_index = torch.nonzero(offsets[i, :, 0] == s)
+                end_index = torch.nonzero(offsets[i, :, 1] == e)
+                if len(start_index) > 0 and len(end_index) > 0:
+                    indexes[i, j, :] = torch.tensor(
+                        [start_index[0], end_index[0]], device=self.device
+                    )
+        return indexes
+
+    def find_subword_indexes(self, word_ids):
+        """
+        Given a list of word IDs, return two mask,
+        one indicating the start of words and one
+        indicating the end of words, which should
+        be used to ignore subwords
+        """
+        start_mask = torch.full(
+            (len(word_ids), len(word_ids[0])), False, device=self.device
+        )
+        end_mask = torch.full_like(start_mask, False, device=self.device)
+
+        for i, word_id in enumerate(word_ids):
+
+            if word_id[0] != None:
+                start_mask[i, 0] = True
+
+            for j in range(1, len(word_id)):
+                if word_id[j] != word_id[j - 1]:
+                    if word_id[j] != None:
+                        start_mask[i, j] = True
+                    if word_id[j - 1] != None:
+                        end_mask[i, j] = True
+
+            if word_id[-1] != None:
+                end_mask[i, -1] = True
+
+        return start_mask, end_mask
+
+    def select_tokenizer(self, entity=None):
+        """
+        Given an entity (None, "context" or "question"),
+        return the corresponding tokenizer
+        """
+        raise NotImplementedError()
+
+    def __call__(self, inputs):
+        raise NotImplementedError()
+
+
+class RecurrentSquadTokenizer(SquadTokenizer):
+    """
+    Tokenizer and data collator to be used with a
+    recurrent-based model, which uses two different tokenizers
+    (one for questions and one for contexts)
+    """
+
+    def __init__(self, question_tokenizer, context_tokenizer, device="cpu"):
+        super().__init__(device=device)
+        assert isinstance(question_tokenizer, Tokenizer)
+        assert isinstance(context_tokenizer, Tokenizer)
+        self.question_tokenizer = question_tokenizer
+        self.context_tokenizer = context_tokenizer
+
+    def select_tokenizer(self, entity=None):
+        """
+        Return the "question" or "context" tokenizer,
+        based on the given entity
+        """
+        assert entity in ("question", "context")
+        return (
+            self.context_tokenizer if entity == "context" else self.question_tokenizer
+        )
+
+    def __call__(self, inputs):
+        zipped_inputs = tuple(zip(*inputs))
+        if len(zipped_inputs) > 3:
+            (indexes, questions, contexts, answers_start, answers_end) = zipped_inputs
+            testing = False
+        else:
+            (indexes, questions, contexts) = zipped_inputs
+            testing = True
+        tokenized_questions = self.tokenize(questions, entity="question", special=True)
+        tokenized_contexts = self.tokenize(contexts, entity="context", special=True)
+        qattr = list(zip(*[self.ATTRGETTER(e) for e in tokenized_questions]))
+        cattr = list(zip(*[self.ATTRGETTER(e) for e in tokenized_contexts]))
+
+        # Create the batch dictionary with encoding info
+        batch = {
+            "question_ids": torch.tensor(
+                qattr[self.ENCODING_ATTR_ID["ids"]], device=self.device
+            ),
+            "question_type_ids": torch.tensor(
+                qattr[self.ENCODING_ATTR_ID["type_ids"]], device=self.device
+            ),
+            "question_attention_mask": torch.tensor(
+                qattr[self.ENCODING_ATTR_ID["attention_mask"]],
+                dtype=torch.bool,
+                device=self.device,
+            ),
+            "question_special_tokens_mask": torch.tensor(
+                qattr[self.ENCODING_ATTR_ID["special_tokens_mask"]],
+                dtype=torch.bool,
+                device=self.device,
+            ),
+            "context_ids": torch.tensor(
+                cattr[self.ENCODING_ATTR_ID["ids"]], device=self.device
+            ),
+            "context_type_ids": torch.tensor(
+                cattr[self.ENCODING_ATTR_ID["type_ids"]], device=self.device
+            ),
+            "context_attention_mask": torch.tensor(
+                cattr[self.ENCODING_ATTR_ID["attention_mask"]],
+                dtype=torch.bool,
+                device=self.device,
+            ),
+            "context_special_tokens_mask": torch.tensor(
+                cattr[self.ENCODING_ATTR_ID["special_tokens_mask"]],
+                dtype=torch.bool,
+                device=self.device,
+            ),
+            "context_offsets": torch.tensor(
+                cattr[self.ENCODING_ATTR_ID["offsets"]], device=self.device
+            ),
+            "indexes": torch.tensor(indexes, dtype=torch.long, device=self.device),
+        }
+
+        # Add custom info to the batch dict
+        batch["context_offsets"] = torch.where(
+            batch["context_attention_mask"].unsqueeze(-1).repeat(1, 1, 2),
+            batch["context_offsets"],
+            -100,
+        )
+        batch["question_lenghts"] = torch.count_nonzero(
+            batch["question_attention_mask"], dim=1
+        )
+        batch["context_lenghts"] = torch.count_nonzero(
+            batch["context_attention_mask"], dim=1
+        )
+        (
+            batch["subword_start_mask"],
+            batch["subword_end_mask"],
+        ) = self.find_subword_indexes(cattr[self.ENCODING_ATTR_ID["word_ids"]])
+
+        if not testing:
+            batch["answers"] = self.find_tokenized_answer_indexes(
+                batch["context_offsets"], answers_start, answers_end
+            )
+        return batch
+
+
+class TransformerSquadTokenizer(SquadTokenizer):
+    """
+    Tokenizer and data collator to be used with
+    a Transformer-based model, which uses a single
+    tokenizer (for the concatenation of question and context)
+    """
+
+    def __init__(self, tokenizer, device="cpu"):
+        super().__init__(device=device)
+        assert isinstance(tokenizer, Tokenizer) or isinstance(tokenizer, BaseTokenizer)
+        self.tokenizer = tokenizer
+
+    def select_tokenizer(self, entity=None):
+        """
+        Ignore the entity parameter and always return
+        the same tokenizer
+        """
+        return self.tokenizer
+
+    def __call__(self, inputs):
+        zipped_inputs = tuple(zip(*inputs))
+        if len(zipped_inputs) > 3:
+            (indexes, questions, contexts, answers_start, answers_end) = zipped_inputs
+            testing = False
+        else:
+            (indexes, questions, contexts) = zipped_inputs
+            testing = True
+
+        tokenized = self.tokenize(list(zip(questions, contexts)), special=True)
+        attr = list(zip(*[self.ATTRGETTER(e) for e in tokenized]))
+
+        # Create the batch dictionary with encoding info
+        batch = {
+            "context_ids": torch.tensor(
+                attr[self.ENCODING_ATTR_ID["ids"]], device=self.device
+            ),
+            "context_type_ids": torch.tensor(
+                attr[self.ENCODING_ATTR_ID["type_ids"]], device=self.device
+            ),
+            "attention_mask": torch.tensor(
+                attr[self.ENCODING_ATTR_ID["attention_mask"]],
+                dtype=torch.bool,
+                device=self.device,
+            ),
+            "special_tokens_mask": torch.tensor(
+                attr[self.ENCODING_ATTR_ID["special_tokens_mask"]],
+                dtype=torch.bool,
+                device=self.device,
+            ),
+            "offsets": torch.tensor(
+                attr[self.ENCODING_ATTR_ID["offsets"]], device=self.device
+            ),
+            "indexes": torch.tensor(indexes, dtype=torch.long, device=self.device),
+        }
+
+        # Add custom info to the batch dict
+        batch["context_attention_mask"] = (
+            batch["context_type_ids"].bool() & ~batch["special_tokens_mask"]
+        )
+        batch["context_offsets"] = torch.where(
+            batch["context_attention_mask"].unsqueeze(-1).repeat(1, 1, 2),
+            batch["offsets"],
+            -100,
+        )
+        (
+            batch["subword_start_mask"],
+            batch["subword_end_mask"],
+        ) = self.find_subword_indexes(attr[self.ENCODING_ATTR_ID["word_ids"]])
+
+        if not testing:
+            batch["answers"] = self.find_tokenized_answer_indexes(
+                batch["context_offsets"], answers_start, answers_end
+            )
+        return batch
+
+
+def get_transformer_tokenizer(vocab_path, max_tokens, device="cpu"):
+    """
+    Return a tokenizer to be used with Transformer-based models
+    """
+    wp_tokenizer = BertWordPieceTokenizer(vocab_path, lowercase=True)
+    wp_tokenizer.enable_padding(direction="right", pad_type_id=1)
+    wp_tokenizer.enable_truncation(max_tokens)
+    return TransformerSquadTokenizer(wp_tokenizer, device=device)
+
+
+def get_recurrent_tokenizer(
+    vocab, max_context_tokens, unk_token, pad_token, device="cpu"
+):
+    """
+    Return a tokenizer to be used with recurrent-based models
+    """
+    question_tokenizer = Tokenizer(WordLevel(vocab, unk_token=unk_token))
+    question_tokenizer.normalizer = Sequence([StripAccents(), Lowercase(), Strip()])
+    question_tokenizer.pre_tokenizer = PreSequence([Whitespace(), Punctuation()])
+    question_tokenizer.enable_padding(
+        direction="right", pad_id=vocab[pad_token], pad_type_id=1, pad_token=pad_token
+    )
+
+    context_tokenizer = Tokenizer(WordLevel(vocab, unk_token=unk_token))
+    context_tokenizer.normalizer = Sequence([StripAccents(), Lowercase(), Strip()])
+    context_tokenizer.pre_tokenizer = PreSequence([Whitespace(), Punctuation()])
+    context_tokenizer.enable_padding(
+        direction="right",
+        pad_id=vocab[pad_token],
+        pad_type_id=1,
+        pad_token=pad_token,
+    )
+    context_tokenizer.enable_truncation(max_context_tokens)
+
+    return RecurrentSquadTokenizer(question_tokenizer, context_tokenizer, device=device)
